aec46af7dbc44d0c82136fb1137ab7a93d557ac1
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-13 08:27:55 -0800

    Test: create #add_department() test

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -3,10 +3,14 @@ require './lib/department'
 require './lib/employee'
 
 describe Budget do
+  let(:budget) {Budget.new(2023)}
+  
   let(:customer_service) {Department.new("Customer Service")}
+  let(:marketing) {Department.new("Marketing")}
+  let(:sales) {Department.new("Sales")}
+
   let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
   let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
-  let(:budget) {Budget.new(2023)}
 
   describe '#initialize' do
     it 'exists' do
@@ -19,6 +23,16 @@ describe Budget do
 
     it 'starts with no departments' do
       expect(budget.departments).to eq([])
+    end
+  end
+
+  describe '#add_department()' do
+    it 'adds departments to the budget' do
+      budget.add_department(customer_service)
+      budget.add_department(marketing)
+      budget.add_department(sales)
+
+      expect(budget.departments).to eq([customer_service, marketing, sales])
     end
   end
 end
