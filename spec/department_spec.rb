9b98d345c4dde1ce9c1a7b2a00464fbc414644fd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-13 08:12:57 -0800

    Test: create #initialize tests

---
+++ spec/department_spec.rb
@@ -0,0 +1,22 @@
+require './lib/department'
+require './lib/employee'
+
+describe Department do
+  let(:customer_service) {Department.new("Customer Service")}
+  let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
+  let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(customer_service).to be_a(Department)
+    end
+
+    it 'has a name' do
+      expect(customer_service.name).to eq("Customer Service")
+    end
+
+    it 'starts with no employees' do
+      expect(customer_service.employees).to eq([])
+    end
+  end
+end
